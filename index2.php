f4c41428d115332c0ed796725dcb064a279227fe
marcocaza <cazamarco3@gmail.com>
2021-05-31 19:06:59 -0500

    Merge branch 'main' of https://github.com/joelomar15/SAGGA

--- index2.php
+++ index2.php
@@ -3,7 +3,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>SAGGA</title>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
-<!-- <script type="text/javascript" src="js/validaciones.js"></script> -->
+<script type="text/javascript" src="js/validaciones.js"></script>
 <link rel="stylesheet" href="css/estilos.css">
 <script type="text/javascript">
   function borrar(obj) {
@@ -22,7 +22,7 @@
   <form name='form1' id='form1' action='guardar.php' method='post'>
     <input name="btnInsertar" id="btnInsertar" type="button" value="Insertar" class="" /></td>
     <table width='90%' height='' align='center' class='tabl' id="tblDatos">
-      <tbody id="tabla">
+      <thead>
         <tr>
           <th rowspan="2">Apellidos y Nombres</th>
           <th rowspan="2">Parentesco</th>
@@ -37,75 +37,14 @@
           <th>NO</th>
           <th>TIPO</th>
         </tr>
-      </tbody>
- 
+      </thead>
+      <thead id="tabla">
+      </thead>
     </table>
 
   
   
   </form>
- <script>
-   var contador=0;
-    
-    $("#btnInsertar").click(function(){
-        if(contador==0){
-          contador=1;
-          addFila();	
-        }else{
-          var verdad=false;
-          var stringValidar="";
-          //alert(contador);
-          for(let i=1;i<contador;i++){
-            if(($("#nombreApellido"+i).val()==""||$("#parentesco"+i).val()==""||$("#edad"+i).val()==""||$("#ProfesionOcupacion"+i).val()=="")||(!$("input[name='Dis-Enf"+i+"']").is(':checked'))){
-              verdad=false;
-            }else{
-              var radioDisEnfer = $('input[name=Dis-Enf'+i+']:checked', '#form1').val();
-              if(radioDisEnfer=="No" && $("#tipo"+i).val()==""){
-                verdad=true;
-              }else if(radioDisEnfer=="Si" && $("#tipo"+i).val()==""){
-                verdad=false;   
-              }else{
-                verdad=true;
-              }
-            }
-            stringValidar=stringValidar+"-"+verdad;
-          }
-          //alert(stringValidar);
-          
-          if(verdad&&stringValidar.includes("false")==false){
-            addFila();
-          }else{
-            alert("Existen campos en la tabla que estan vacios\nVerifique...");
-          }
-        }
-    });	
-  
-function addFila(){
-  
-  var tablaDatos= $("#tblDatos");	
-  
-  tablaDatos.append("<tr><td><input type='text' name='nombreApellido"+contador+"' id='nombreApellido"+contador+"' class='' /></td><td><input type='text' name='parentesco"+contador+"' id='parentesco"+contador+"' class='' /></td><td><input type='text' name='edad"+contador+"' id='edad"+contador+"' class='' /></td><td><input type='text'  name='ProfesionOcupacion"+contador+"' id='ProfesionOcupacion"+contador+"' class='' /></td><td><input type='radio' id='siDis-Enf"+contador+"' name='Dis-Enf"+contador+"' value='Si' class='validarTipo"+contador+"' ></td><td><input type='radio' id='noDis-Enf"+contador+"' name='Dis-Enf"+contador+"' value='No' class='validarTipo"+contador+"' ></td><td><input type='text' name='tipo"+contador+"' id='tipo"+contador+"' class='activarTipo"+contador+"' /></td><td><input type='button' class='Eliminar' onclick='borrar(this)' value='ELIMINAR' /></td></tr>");
-  var classTipo=".activarTipo"+contador;
-  $(classTipo).prop('disabled', true);
-  var classOption = ".validarTipo"+contador;
-  $(classOption).click(function(event) {
-      if($(this).val()=="Si"){
-          $(classTipo).prop('disabled', false);
-      }else{
-          $(classTipo).prop('disabled', true);
-          $(classTipo).val("");
-      }
-  });
-  contador++;
-}
-function borrar(obj) {
-    fila = obj.parentNode.parentNode;
-    document.getElementById('tabla').removeChild(fila);
-    contador--;
-    if(contador==1){
-      contador=0;
-    }
-  }
- </script>
+  <script type="text/javascript" src="js/validaciones.js"></script>
 </body>
 </html>
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,57 +1,64 @@
-$(document).ready(function(){
-    var contador=0;
-    
-        $("#btnInsertar").click(function(){
-            if(contador==0){
-              contador=1;
-              addFila();	
-            }else{
-              var verdad=false;
-              var stringValidar="";
-              //alert(contador);
-              for(let i=1;i<contador;i++){
-                if(($("#nombreApellido"+i).val()==""||$("#parentesco"+i).val()==""||$("#edad"+i).val()==""||$("#ProfesionOcupacion"+i).val()=="")||(!$("input[name='Dis-Enf"+i+"']").is(':checked'))){
-                  verdad=false;
-                }else{
-                  var radioDisEnfer = $('input[name=Dis-Enf'+i+']:checked', '#form1').val();
-                  if(radioDisEnfer=="No" && $("#tipo"+i).val()==""){
-                    verdad=true;
-                  }else if(radioDisEnfer=="Si" && $("#tipo"+i).val()==""){
-                    verdad=false;   
-                  }else{
-                    verdad=true;
-                  }
-                }
-                stringValidar=stringValidar+"-"+verdad;
-              }
-              //alert(stringValidar);
-              
-              if(verdad&&stringValidar.includes("false")==false){
-                addFila();
-              }else{
-                alert("Existen campos en la tabla que estan vacios\nVerifique...");
-              }
-            }
-        });	
-      
-    function addFila(){
-      
-      var tablaDatos= $("#tblDatos");	
-      
-      tablaDatos.append("<tr><td><input type='text' name='nombreApellido"+contador+"' id='nombreApellido"+contador+"' class='' /></td><td><input type='text' name='parentesco"+contador+"' id='parentesco"+contador+"' class='' /></td><td><input type='text' name='edad"+contador+"' id='edad"+contador+"' class='' /></td><td><input type='text'  name='ProfesionOcupacion"+contador+"' id='ProfesionOcupacion"+contador+"' class='' /></td><td><input type='radio' id='siDis-Enf"+contador+"' name='Dis-Enf"+contador+"' value='Si' class='validarTipo"+contador+"' ></td><td><input type='radio' id='noDis-Enf"+contador+"' name='Dis-Enf"+contador+"' value='No' class='validarTipo"+contador+"' ></td><td><input type='text' name='tipo"+contador+"' id='tipo"+contador+"' class='activarTipo"+contador+"' /></td><td><input type='button' class='Eliminar' onclick='borrar(this)' value='ELIMINAR' /></td></tr>");
-      var classTipo=".activarTipo"+contador;
-      $(classTipo).prop('disabled', true);
-      var classOption = ".validarTipo"+contador;
-      $(classOption).click(function(event) {
-          if($(this).val()=="Si"){
-              $(classTipo).prop('disabled', false);
-          }else{
-              $(classTipo).prop('disabled', true);
-              $(classTipo).val("");
-          }
-      });
-      contador++;
-    }
 
-    
-});
+  var contador=0;
+  //boton  insertar y validaciones.
+  $("#btnInsertar").click(function(){
+      if(contador==0){
+        contador=1;
+        addFila();	
+      }else{
+        var verdad=false;
+        var stringValidar="";
+        for(let i=1;i<contador;i++){
+          //validacion si los campos estan vacios
+          if(($("#nombreApellido"+i).val()==""||$("#parentesco"+i).val()==""||$("#edad"+i).val()==""||$("#ProfesionOcupacion"+i).val()=="")||(!$("input[name='Dis-Enf"+i+"']").is(':checked'))){
+            verdad=false;
+          }else{
+            var radioDisEnfer = $('input[name=Dis-Enf'+i+']:checked', '#form1').val();
+            //validacion de los input radio
+            if(radioDisEnfer=="No" && $("#tipo"+i).val()==""){
+              verdad=true;
+            }else if(radioDisEnfer=="Si" && $("#tipo"+i).val()==""){
+              verdad=false;   
+            }else{
+              verdad=true;
+            }
+          }
+          stringValidar=stringValidar+"-"+verdad;
+        }
+        //validacion con los retornos de las validaciones
+        if(verdad&&stringValidar.includes("false")==false){
+          addFila();
+        }else{
+          alert("Existen campos en la tabla que estan vacios\nVerifique...");
+        }
+      }
+  });	
+//funcion añadir fila de la tabla y validaciones
+function addFila(){
+  var tablaDatos= $("#tabla");	
+  tablaDatos.append("<tr><td><input type='text' name='nombreApellido"+contador+"' id='nombreApellido"+contador+"' class='' /></td><td><input type='text' name='parentesco"+contador+"' id='parentesco"+contador+"' class='' /></td><td><input type='text' name='edad"+contador+"' id='edad"+contador+"' class='' /></td><td><input type='text'  name='ProfesionOcupacion"+contador+"' id='ProfesionOcupacion"+contador+"' class='' /></td><td><input type='radio' id='siDis-Enf"+contador+"' name='Dis-Enf"+contador+"' value='Si' class='validarTipo"+contador+"' ></td><td><input type='radio' id='noDis-Enf"+contador+"' name='Dis-Enf"+contador+"' value='No' class='validarTipo"+contador+"' ></td><td><input type='text' name='tipo"+contador+"' id='tipo"+contador+"' class='activarTipo"+contador+"' /></td><td><input type='button' onclick='borrar(this)' value='ELIMINAR' /></td></tr>");
+  var classTipo=".activarTipo"+contador;
+  $(classTipo).prop('disabled', true);
+  var classOption = ".validarTipo"+contador;
+  $(classOption).click(function(event) {
+    //validamos los valores de los input radio y modificar el valor del tipo
+    if($(this).val()=="Si"){
+        $(classTipo).prop('disabled', false);
+    }else{
+        $(classTipo).prop('disabled', true);
+        $(classTipo).val("");
+    }
+  });
+  contador++;
+}
+
+
+//funcion borrar filas de la tabla y validaciones
+function borrar(obj) {
+  fila = obj.parentNode.parentNode;
+  document.getElementById('tabla').removeChild(fila);
+  contador--;
+  if(contador==1){
+    contador=0;
+  }
+} 
